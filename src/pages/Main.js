29061a3494512eab468f3a3915b41c07bd3bda47
ThePinakiRoy <roypin49@gmail.com>
2020-10-11 13:37:20 +0530

    dynamic add & removal

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,9 +9,12 @@ export class Main extends Component {
         this.state = {
             data: [],
             loading: true,
-            numBox: 1
+            numBox: 12,
+            children:[]
         }
-        this.addNewVideoBox = this.addNewVideoBox.bind(this)
+        this.addNewVideoBox = this
+            .addNewVideoBox
+            .bind(this)
     }
 
     componentDidMount() {
@@ -28,33 +31,59 @@ export class Main extends Component {
         });
     }
     addNewVideoBox() {
+        const {data} = this.state;
         this.setState({
             numBox: this.state.numBox + 1
-          });
+        });
+        const randomNum = Math.floor(Math.random() * data.length);
+        const randomHash = Math.floor(Math.random() * 25);
+                let item = data[randomNum];
+                if (item["count"] < 5) {
+                    data[randomNum]["count"] = data[randomNum]["count"] + 1;
+                    data[randomNum]["id"] = data[randomNum]["count"]+data[randomNum]["url"]
+                    let className = 'badge-success'
+                    if (data[randomNum]["count"] == 1 || data[randomNum]["count"] == 2) {
+                        className = 'badge-success'
+                    }
+                    if (data[randomNum]["count"] == 3 || data[randomNum]["count"] == 4) {
+                        className = 'badge-warning'
+                    }
+                    if (data[randomNum]["count"] == 5) {
+                        className = 'badge-danger'
+                    }
+                    this.state.children.push(<VideoBox
+                        key={item["url"]+randomHash}
+                        number={randomNum}
+                        url={item["url"]}
+                        id={data[randomNum]["id"]}
+                        count={item["count"]}
+                        className={className}
+                        removeBox={this.removeBox.bind(this)}/>);
+                }
+                console.log('c',this.state.children)
     }
-    removeLocalStorage() {
-
+    removeBox (id) {
+        console.log('remove box ',id)
+        const newState = this.state;
+        const index = newState.children.findIndex(a => a.props.id === id);
+        console.log(index)
+        if (index === -1) return;
+        newState.children.splice(index, 1);
+    
+        this.setState(newState)
     }
+    removeLocalStorage() {}
     render() {
-        const {data, loading ,numBox } = this.state;
-        
-        const children = [];
+        const { loading} = this.state;
+       
         let load = true;
-        if(!loading){
-            
-        for (var i = 0; i < numBox; i += 1) {
-            data.filter(dt => (dt["count"] < 5) )
-            children.push(<VideoBox key={i} number={i} url={data["url"]}/>);
-        };
-        load = false;
-        } else {
-            console.log("len before",data.length)
-        console.log(loading)
-        }
+        if (!loading) {
+            load = false;
+        } 
         return (
             <div>
                 {/* <Header /> */}
-             
+
                 <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
                     <a className="navbar-brand" href="#">ut-Access</a>
                     <button
@@ -69,9 +98,10 @@ export class Main extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            
+
                             <li className="nav-item active">
-                                <a className="nav-link" onClick={this.addNewVideoBox}>Add Box </a>
+                                <a className="nav-link" onClick={this.addNewVideoBox}>Add Box
+                                </a>
                             </li>
                             <li className="nav-item active">
                                 <a className="nav-link" onClick={this.removeLocalStorage}>Clear LS</a>
@@ -82,8 +112,11 @@ export class Main extends Component {
                 </nav>
                 {/* container */}
                 <div className="container pt-5">
+                    {this.state.children.length ==0 ? <h1>Click on Add Box</h1>:<React.Fragment></React.Fragment>}
                     <div className="row">
-                    {!load ? children : <p>getting data</p> }
+                        {!load
+                            ? this.state.children
+                            : <p>getting data</p>}
                     </div>
                 </div>
 
