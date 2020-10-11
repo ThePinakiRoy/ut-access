3400d8ea8de22f2b9be71481c36b48602a86ecd4
ThePinakiRoy <roypin49@gmail.com>
2020-10-11 13:48:11 +0530

    Updates

---
+++ precache-manifest.af16523d358e741031cffb693817a278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cc0416cf7231ec382623cc06f7d6ef",
+    "url": "/ut-access/index.html"
+  },
+  {
+    "revision": "69f3c5cd501b3b83a867",
+    "url": "/ut-access/static/css/main.35d03fd8.chunk.css"
+  },
+  {
+    "revision": "a12e1e5f8ba730d40930",
+    "url": "/ut-access/static/js/2.76ea4bfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ut-access/static/js/2.76ea4bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f3c5cd501b3b83a867",
+    "url": "/ut-access/static/js/main.99c265e9.chunk.js"
+  },
+  {
+    "revision": "17b75976e05884f1ca3e",
+    "url": "/ut-access/static/js/runtime-main.74a438d4.js"
+  }
+]);
