026531e01fa327375413a459cf0780ec8ec974e9
ThePinakiRoy <roypin49@gmail.com>
2021-02-03 12:59:25 +0530

    Updates

--- precache-manifest.3271da614c90067fbfe3e5594a431e02.js
+++ precache-manifest.3271da614c90067fbfe3e5594a431e02.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dac8b2d162adab59fb9ac3a5028677c9",
+    "revision": "ce2eeb75d14c847a1944cfb2225dad34",
     "url": "/ut-access/index.html"
   },
   {
-    "revision": "39729f6443576d133e6d",
+    "revision": "0eb8c06afc5ccbba31b5",
     "url": "/ut-access/static/css/main.d1eb618d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ut-access/static/js/2.d9d84082.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "39729f6443576d133e6d",
-    "url": "/ut-access/static/js/main.e22dd269.chunk.js"
+    "revision": "0eb8c06afc5ccbba31b5",
+    "url": "/ut-access/static/js/main.c5d0d4c2.chunk.js"
   },
   {
     "revision": "17b75976e05884f1ca3e",
